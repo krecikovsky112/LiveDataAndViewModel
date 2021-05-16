e70c059778ffb81e063ec45b30d76057132e49a5
krecikovsky112 <krecikovsky112@gmail.com>
2021-05-16 19:49:37 +0200

    Clean code, refactor

--- app/src/main/java/com/example/livedataapp/MainActivity.java
+++ app/src/main/java/com/example/livedataapp/MainActivity.java
@@ -4,35 +4,24 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.lifecycle.*;
-import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private MainActivityViewModel model;
     private TextView nameTextView;
     private Button button;
 
-    private String TAG = this.getClass().getSimpleName();
+    private final String TAG = this.getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.buttonRefresh);
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        setComponents();
 
-        nameTextView = findViewById(R.id.nameTextView);
         final MainActivityViewModel model = new ViewModelProvider(this).get(MainActivityViewModel.class);
         final MutableLiveData<String> myRandomNumber = model.getNumber();
         myRandomNumber.observe(this, new Observer<String>() {
@@ -52,5 +41,10 @@ public class MainActivity extends AppCompatActivity {
 
 
         Log.i(TAG, "Random Number Set");
+    }
+
+    private void setComponents() {
+        button = findViewById(R.id.buttonRefresh);
+        nameTextView = findViewById(R.id.nameTextView);
     }
 }
